525005301d227546e3b4c2d2422e0564bc62be77
zhaoxuetong <zhaoxuetong@chinahomu.com>
2016-12-23 17:55:59 +0800

    2016-12-23 changes

---
+++ app/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {RouteHandler} from 'react-router';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <RouteHandle />
+            </div>
+        );
+    }
+}
+
+export default App;
